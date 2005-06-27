88ef4408ffe2473086699d9bc9e28f736b826e2d
Alban Bedel <albeu@free.fr>
2005-06-27 19:49:20 +0000

    Add the GUI variables.

--- scummVars6.s
+++ scummVars6.s
@@ -76,6 +76,49 @@ int VAR_LEFTBTN_HOLD           @ 74;
 int VAR_RIGHTBTN_HOLD          @ 75;
 int VAR_V6_EMSSPACE            @ 76;
 
+// "Inserez la disquette %c et cliquez sur la souris."
+array VAR_GAME_DISK_MSG        @ 90;
+// "Impossible d'ouvrir %s, (%c%d) Cliquez sur la souris."
+array VAR_OPEN_FAILED_MSG      @ 91;
+// "Erreur de lecture disque %c, (%c%d) Cliquez sur la souris."
+array VAR_READ_ERROR_MSG       @ 92;
+// "Pause. Appuyez sur ESPACE pour continuer."
+array VAR_PAUSE_MSG            @ 93;
+// "Vous voulez vraiment recommencer?  (O/N)o"
+array VAR_RESTART_MSG          @ 94;
+// "Vous voulez vraiment quitter le jeu?  (O/N)O"
+array VAR_QUIT_MSG             @ 95;
+// "Sauver"
+array VAR_SAVE_BTN             @ 96;
+// "Charger"
+array VAR_LOAD_BTN             @ 97;
+// "Jouer"
+array VAR_PLAY_BTN             @ 98;
+// "Annuler"
+array VAR_CANCEL_BTN           @ 99;
+// "Quitter"
+array VAR_QUIT_BTN             @ 100;
+// "OK"
+array VAR_OK_BTN               @ 101;
+// "Inserez votre disquette de sauvegardes"
+array VAR_SAVE_DISK_MSG        @ 102;
+// "Vous devez entrer un nom"
+array VAR_ENTER_NAME_MSG       @ 103;
+// "Partie NON sauvegardee (disque plein?)"
+array VAR_NOT_SAVED_MSG        @ 104;
+// "Partie NON chargee"
+array VAR_NOT_LOADED_MSG       @ 105;
+// "Sauvegarde de '%s'"
+array VAR_SAVE_MSG             @ 106;
+// "Chargement de '%s'"
+array VAR_LOAD_MSG             @ 107;
+// "Entrez le nom de la SAUVEGARDE"
+array VAR_SAVE_MENU_TITLE      @ 108;
+// "Entrez le nom de la partie a CHARGER"
+array VAR_LOAD_MENU_TITLE      @ 109;
+
+// "Que desirez-vous?"
+array VAR_MAIN_MENU_TITLE      @ 117
 int VAR_RANDOM_NR              @ 118;
 int VAR_TIMEDATE_YEAR          @ 119;
 
