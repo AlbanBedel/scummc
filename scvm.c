b7518fc1764bd3cc4bacc66f5d5fb065c09ce884
Alban Bedel <albeu@free.fr>
2007-12-17 00:30:38 +0000

    Set the initial camera position when opening a room. Fix the jumping camera in the 3 room of dott.

--- scvm.c
+++ scvm.c
@@ -609,6 +609,7 @@ int scvm_run_threads(scvm_t* vm,unsigned cycles) {
         vm->var->camera_max_x = room->width - vm->var->camera_min_x;
         vm->var->room = room->id;
         vm->room = room;
+        vm->view->camera_x = vm->var->camera_min_x;
         vm->state = SCVM_RUN_PRE_ENTRY;
       }
     case SCVM_RUN_PRE_ENTRY:
